0572e1044c7eb3681b4a0cdc0831a415af06d43c
chana <chana.black@gmail.com>
2013-05-02 17:07:39 -0400

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -86,23 +86,6 @@ describe "User pages" do
       end
     end
 
-    describe "with setting admin attribute" do
-      before do
-        post("users", { name: "Example User", 
-                        email: "admintestuser@example.com", 
-                        password: "foobar",
-                        confirmation: "foobar",
-                        admin: "true" } )   
-        #@user = User.find_by_email('admin@example.com')
-        
-      end
-
-      #it "should not set admin for new user" do
-      #  user.admin.should == false
-      #  response.should_not be_success
-      #end
-    end
-
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -162,4 +145,22 @@ describe "User pages" do
       specify { user.reload.email.should == new_email }
     end
   end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
+  
 end
